f6716d807c72506753df9e9b2485c86575fd2a23
Jeff Barragan <leshz@me.com>
2024-08-27 10:31:50 -0500

    update name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>SHZ.CODE</h1>
+      <h1>SHZTECH.DEV</h1>
       <h2>Under constuction 🚧</h2>
       <h3>⚠️⚠️WIP⚠️⚠️</h3>
     </main>
